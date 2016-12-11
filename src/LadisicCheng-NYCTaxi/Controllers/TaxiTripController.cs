d959d8bcdc429962e2ef8bd4fea74a89f4c54dbe
Josh Ladisic <jal5313@gmail.com>
2016-12-11 18:11:26 -0500

    Add basic CRUD operations

--- src/LadisicCheng-NYCTaxi/Controllers/TaxiTripController.cs
+++ src/LadisicCheng-NYCTaxi/Controllers/TaxiTripController.cs
@@ -12,5 +12,64 @@ namespace LadisicCheng_NYCTaxi.Controllers
             TaxiTrips = taxiTrips;
         }
         public ITaxiTripRepository TaxiTrips { get; set; }
+
+        [HttpGet]
+        public IEnumerable<TaxiTrip> GetAll()
+        {
+            return TaxiTrips.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetTrip")]
+        public IActionResult GetById(string id)
+        {
+            var item = TaxiTrips.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TaxiTrip item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            TaxiTrips.Add(item);
+            return CreatedAtRoute("GetTrip", new { id = item.Key }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] TaxiTrip item)
+        {
+            if (item == null || item.Key != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = TaxiTrips.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TaxiTrips.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var todo = TaxiTrips.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TaxiTrips.Remove(id);
+            return new NoContentResult();
+        }
     }
 }
